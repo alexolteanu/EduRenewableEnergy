c9a9b3b49ee35c5b1163056ee4254e1e3f9f2cc0
Alexandru Olteanu <alex.c.olteanu@gmail.com>
2018-12-05 19:48:27 +0200

    basic persistance using Hibernate and MySQL see https://spring.io/guides/gs/accessing-data-mysql/

---
+++ src/main/java/hello/model/UserRepository.java
@@ -0,0 +1,12 @@
+package hello.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+import hello.model.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
